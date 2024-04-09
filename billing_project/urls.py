4b8c93e512581f6871e15c7b959ea381bcbba876
kiran <96545669+kiranj100@users.noreply.github.com>
2024-04-09 17:53:33 +0530

    required some changes

--- billing_project/urls.py
+++ billing_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
-from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -21,5 +21,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('billing_app.urls')),
+    path('', lambda request: redirect('schema-swagger-ui', permanent=False)),  # Redirect root URL to Swagger UI URL
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
